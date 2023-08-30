7b96cf976f1016dfea60ac2d272276bbcfa1cd0d
Brendom Gonçalves <brendom.vi@gmail.com>
2023-08-29 21:44:00 -0300

    Programação Imperativa

---
+++ PI-P001/Projeto1/meuprog1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    char nome[20];
+    cout << "Digite seu nome: " << endl << ">";
+    cin >> nome;
+    cout << "Bem vindo " << nome << "!" << endl;
+    return 0;
+}
---
+++ PI-P001/Projeto1/meuprog2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    float A, B;
+
+    cin >> A;
+    cin >> B;
+    
+    cout << "Soma = " << A + B << endl;
+    cout << "Subtração = " << A - B << endl;
+    cout << "Multiplicação = " << A * B << endl;
+    cout << "Divisão = " << A / B << endl;
+
+    return 0;
+}
